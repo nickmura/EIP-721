4bdd79881da71560e30734248ed0469748a87ac8
nickmura <92566574+nickmura@users.noreply.github.com>
2022-09-28 21:32:50 -0600

    Removed IERC721TokenReciever.sol inheritance

--- IERC721.sol
+++ IERC721.sol
@@ -3,11 +3,6 @@ pragma solidity ^0.8.10;
 
 interface IERC721 {
 
-    /// @dev This emits when ownership of any NFT changes by any mechanism.
-    ///  This event emits when NFTs are created (`from` == 0) and destroyed
-    ///  (`to` == 0). Exception: during contract creation, any number of NFTs
-    ///  may be created and assigned without emitting Transfer. At the time of
-    ///  any transfer, the approved address for that NFT (if any) is reset to none.
     event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
 
     /// @dev This emits when the approved address for an NFT is changed or
